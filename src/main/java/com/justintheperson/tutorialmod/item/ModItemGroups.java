e6f7e8ee1781c73e431eba864ae4a3e6db3ef98d
Justin <worklarite@gmail.com>
2023-07-21 01:01:13 +0200

    stuff

--- src/main/java/com/justintheperson/tutorialmod/item/ModItemGroups.java
+++ src/main/java/com/justintheperson/tutorialmod/item/ModItemGroups.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
--- src/main/java/com/justintheperson/tutorialmod/item/custom/eightball/EightBallItem.java
+++ src/main/java/com/justintheperson/tutorialmod/item/custom/eightball/EightBallItem.java
@@ -1,13 +1,19 @@
 package com.justintheperson.tutorialmod.item.custom.eightball;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Settings settings) {
@@ -23,6 +29,18 @@ public class EightBallItem extends Item {
 
         return super.use(world, user, hand);
     }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right click to get a random number between 1 and 8!").formatted(Formatting.YELLOW));
+        } else {
+            tooltip.add(Text.literal("Press shift for more info!").formatted(Formatting.WHITE));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
     private void outputRandomNumber(PlayerEntity player){
         player.sendMessage(Text.literal("Your Number is " + getRandomNumber()));
     }
